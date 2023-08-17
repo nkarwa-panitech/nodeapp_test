2df6f5a784a88f890ffe45446c51f4a8dbf19492
nkarwa-panitech <121815140+nkarwa-panitech@users.noreply.github.com>
2023-08-17 11:56:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Panitech" }');
+    res.send('{ "response": "Hello From Panitech Academy" }');
 });
 
 app.get('/will', function (req, res) {
